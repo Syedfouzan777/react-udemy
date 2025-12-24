f59386367a29c1b4df15bbfa5e8bb46fc7c7c42c
Syedfouzan777 <syedfouzanofficial@gmail.com>
2025-12-24 08:28:29 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learn React with Next JS</h1>
+      <p>We will Learn React with Next JS and also other things</p>
+    </>
+  );
+}
+
+export default App;
